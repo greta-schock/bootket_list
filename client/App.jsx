9fd9304d5b7adaf07f5824a540a16ea04331cb0b
Greta Schock <gschockcodes@gmail.com>
2021-03-31 15:19:35 -0700

    Reorganized files/components

--- client/App.jsx
+++ client/App.jsx
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import _, { where } from 'underscore';
 //Child Components
-import NPList from './components/NPList.jsx';
-import HikesList from './components/HikesList.jsx';
-import SelectedHikeDetail from './components/SelectedHikeDetail.jsx';
+import ExplorePage from './ExplorePage.jsx';
+// import NPList from './explore_components/NPList.jsx';
+// import HikesList from './explore_components/HikesList.jsx';
+// import SelectedHikeDetail from './explore_components/SelectedHikeDetail.jsx';
 //React Bootstrap Components
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
 import Row from 'react-bootstrap/Row';
 
 const App = () => {
     const [parksInfoArr, setParksInfoArr] = useState([]);
-    const [hikeInfoArr, setHikeInfoArr] = useState([]);
-    const [selectedHikeInfo, setSelectedHikeInfo] = useState({});
-
-    // const [isNPListShowing, setIsNPListShowing] = React.useState(true);
 
     const getParksInfo = () => {
         axios.get('/api/parksInfo')
@@ -29,55 +29,26 @@ const App = () => {
             .catch((err) => console.log(err));
     }
 
-    const getHikesInfo = (parkCode) => {
-        console.log(parkCode)
-        axios.get(`/api/hikingTrailsInfo/${parkCode}`)
-            .then((data) => {
-                console.log('data coming in from the GET request: ', data.data);
-                setHikeInfoArr(data.data);
-            })
-            .catch((err) => console.log(err));
-    }
-
     useEffect(() => getParksInfo(), []);
 
     return (
         <React.Fragment>
-            <Container fluid>
-                <Row><h1>Bootket List</h1></Row>
-                <Row>
-                    <Col>
-
-                        <h3>National Parks</h3>
-                        <Form.Group>
-                            <Form.Control size="sm" type="text" width="25%" placeholder="Search" />
-                        </Form.Group>
-
-                        <NPList
-                            parksInfoArr={parksInfoArr}
-                            getHikesInfo={getHikesInfo}
-                        />
-                    </Col>
-                    <Col>
-                        <Row>
-                            <h3>Hikes List</h3>
-                            <Button>Sort Button</Button>
-
-                        </Row>
-                        <HikesList
-                            hikeInfoArr={hikeInfoArr}
-                            setSelectedHikeInfo={setSelectedHikeInfo}
-                        />
-                    </Col>
-                    <Col>
-                        <h3>Hike Detail</h3>
-                        <SelectedHikeDetail
-                            selectedHikeInfo={selectedHikeInfo}
-                        />
-
-                    </Col>
-                </Row>
-            </Container>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home"><h1>[boot icon here]Bootket List</h1></Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Explore</Nav.Link>
+                    <Nav.Link href="#features">My Bootket List</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+            <ExplorePage
+              parksInfoArr={parksInfoArr}
+            />
+            
+            <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </React.Fragment>
     )
 }

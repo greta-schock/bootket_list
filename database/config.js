5f3b7278f6735b76bbf1623ef63d3f0f3d3c6c94
Greta Schock <gschockcodes@gmail.com>
2021-06-22 15:33:59 -0700

    Added config file

---
+++ database/config.js
@@ -0,0 +1,9 @@
+const POSTGRES_CONNECTION = {
+    user: process.env.USER, 
+    host: process.env.HOST, 
+    // database: 'products_overview', 
+    // password: process.env.PASSWORD, 
+    port: process.env.PORT,
+}
+
+module.exports = POSTGRES_CONNECTION;

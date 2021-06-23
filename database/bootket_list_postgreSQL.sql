5743e690e7e7cf3ef126571919f839727efee61c
Greta Schock <gschockcodes@gmail.com>
2021-06-22 17:11:57 -0700

    Added indexing to PostgreSQL db

--- database/bootket_list_postgreSQL.sql
+++ database/bootket_list_postgreSQL.sql
@@ -1,3 +1,19 @@
 CREATE DATABASE bootket_list; 
 
 \c bootket_list;
+
+-- CREATE TABLES
+CREATE TABLE hikes (
+    id SERIAL NOT NULL PRIMARY KEY,
+    "id": VARCHAR (50),
+    "parkName" VARCHAR (250),
+    "url" VARCHAR (250),
+    "title" VARCHAR (250),
+    "shortDescription" VARCHAR (1000),
+    "accessibilityInformation" VARCHAR (1000),
+    "arePetsPermitted" VARCHAR (250),
+    "duration" VARCHAR (250),
+    "durationDescription" VARCHAR (1000), 
+);
+
+CREATE INDEX id ON hikes (id);
